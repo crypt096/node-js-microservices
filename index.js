9072d38b1948b9ec82555d390fde5be78efa824c
Alex <31933549+crypt096@users.noreply.github.com>
2024-04-13 18:05:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,11 @@ import { ServiceBroker } from 'moleculer';
 const broker = new ServiceBroker();
 
 // Greeter Service
-broker.createService();
+broker.createService({
+  name: 'Greeter',
+  actions: {
+    hello(ctx) {
+      return `Hello ${ctx.params.name}`;
+    }
+  }
+});
