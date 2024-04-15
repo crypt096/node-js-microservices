9b6727960d6978d41453e128f8865bfeb85c59ed
Alex <31933549+crypt096@users.noreply.github.com>
2024-04-15 10:07:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,12 @@ async function startApp() {
       name: 'John Doe',
       email: 'john.doe@example.com',
     });
-    console.log('New user:', newUser);
+    console.log('New user: ', newUser);
     const users = await UserService.call('user.getUsers');
+    console.log('All users: ', users);
   } catch (error) {
-    
+    console.log('Error: ' + error)
+  } finally {
+    await UserService.stop();
   }
 }
